03dac1bd033d8a91e0d4572734fc6e4e25e8dab1
Indira Rajiv Gandhi Karanam <ikaranam@innominds.com>
2018-09-05 17:41:54 +0530

    added

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 //Install express server
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/sampleApp'));
 
-app.get('/*', function(req,res) {
-    
+app.get('/*', function(req,res) {   
 res.sendFile(path.join(__dirname+'/dist/sampleApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen( 8080);
+app.listen( process.env.PORT || 8080);
+
