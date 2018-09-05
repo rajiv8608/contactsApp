14e31f296edf45af586b8c438c667ddc0fc3e846
Indira Rajiv Gandhi Karanam <ikaranam@innominds.com>
2018-09-05 15:09:04 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/contactsApp'));
+app.use(express.static(__dirname + '/dist/sampleApp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/contactsApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/sampleApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
